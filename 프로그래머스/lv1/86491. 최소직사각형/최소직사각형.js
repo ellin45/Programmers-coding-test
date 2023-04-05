36512eae5833976c28122b7f31f6a769af60f63e
ellin45 <119480528+ellin45@users.noreply.github.com>
2023-04-05 13:21:11 +0900

    [level 1] Title: 최소직사각형, Time: 6.00 ms, Memory: 39.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,16 @@
+function solution(sizes) {
+    let arr = sizes.map(size => size[0] > size[1] ? [size[0],size[1]]: [size[1],size[0]]);
+                        
+    const width = [];
+    const height = [];
+    
+    for(let i=0; i<arr.length ; i++){
+        width.push(arr[i][0])
+        height.push(arr[i][1]);
+        
+    }
+        return Math.max(...width)*Math.max(...height);
+    
+}
+// 배열에서 가장 큰값 구하고
+// 배열에서 두번째로 큰 값 구해서 곱하면 됨
