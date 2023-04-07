663af610dfbaa86d641c9ec500708fe4a540c04f
ellin45 <119480528+ellin45@users.noreply.github.com>
2023-04-07 11:37:02 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,8 @@
+function solution(strings, n) {
+    var answer = [];
+    answer = strings.sort((a,b)=>   
+    (a.charCodeAt(n)-b.charCodeAt(n)) == 0 ?  
+       (a > b ? 1 : -1) :                               
+       (a.charCodeAt(n)-b.charCodeAt(n)))                                   
+    return answer;
+}
