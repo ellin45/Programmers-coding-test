758fb4b3dee31a25213ad6b1a02bf5193efe8962
ellin45 <119480528+ellin45@users.noreply.github.com>
2023-04-10 09:48:00 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.81 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let answer =0;
+    let cnt =1;
+    while(true) {
+        if(cnt > n) {
+            break;
+        }
+        if(n % cnt === 0){
+            answer = answer +1;
+        }
+        cnt = cnt+1;
+    }
+    return answer;
+}
+
+
+
