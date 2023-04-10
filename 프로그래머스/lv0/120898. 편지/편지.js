251db975952fe76d718f9c1e67101cce30b0ef14
ellin45 <119480528+ellin45@users.noreply.github.com>
2023-04-10 09:33:52 +0900

    [level 0] Title: 편지, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120898. 편지/편지.js
+++ 프로그래머스/lv0/120898. 편지/편지.js
@@ -1,7 +1,4 @@
 function solution(message) {
-    var answer = (message.length)*2;
+    var answer = (message.length)*2
     return answer;
 }
-//최소 가로 길이는 공백을 포함한 글자의 개수
-//글자의 길이 + 공백의수
-//message.length + ''
