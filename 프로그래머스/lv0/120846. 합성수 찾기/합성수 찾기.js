9f7993f6164fa858ab03d33d3a5225c92b0d7920
정채림 <119480528+ellin45@users.noreply.github.com>
2023-04-13 10:14:25 +0900

    [level 0] Title: 합성수 찾기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,44 @@
+// const getIsCount = (i) => {
+//     let isCount = false;
+//     for(let num =2; num < i-1; num++){
+//         if(i% num === 0) {
+//             //합성수
+//             isCount = true;
+//         }
+//     }
+//     return isCount;
+// }
+
+// function solution(n) {
+//     let answer = 0;
+//     for(let i =n; i >= 1; i--) {
+//         //i가 합성수인지 비교해야하는 숫자
+//         const isCount = getIsCount(i);
+//         // isCount => 합성수 여부를 확인 할 수 있음
+//         if(isCount) {
+//             answer += 1;
+
+//         }
+//     }
+//     return answer;
+// }
+const getIsCount  = (i) => {
+    let isCount = false;
+    for(let num = 2; num<i-1; num++){
+        if(i % num === 0){
+    
+    isCount = true;
+    }
+    }
+return isCount;
+}
+function solution (n){
+    let answer = 0;
+    for(let i = n; i>= 1; i--){
+        const isCount = getIsCount(i);
+        if(isCount) {
+            answer += 1;
+        }
+    }
+    return answer;
+}
