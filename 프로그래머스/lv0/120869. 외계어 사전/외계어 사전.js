663b7dd62061ca9aa681b0fae9bd255e85c8f4ea
정채림 <119480528+ellin45@users.noreply.github.com>
2023-04-19 11:44:01 +0900

    [level 0] Title: 외계어 사전, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,4 +1,8 @@
 function solution(spell, dic) {
-    spell = spell.sort().join("")
-    return dic.map(a=>a.split("").sort().join("")).find(a =>a === spell) !== undefined ? 1 :2
+ spell = spell.sort().join("")
+    return dic.map(a=>a.split('').sort().join("")).find(a=>a === spell) !== undefined ? 1 : 2 
+    console.log(dic)
 }
+//  spell = spell.sort().join("")
+//     return dic.map(a=>a.split("").sort().join("")).find(a =>a === spell) !== undefined ? 1 :2
+// }
