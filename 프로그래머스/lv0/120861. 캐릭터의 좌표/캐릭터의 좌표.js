8b45f242bc696dac3901679b65285650951510f4
정채림 <119480528+ellin45@users.noreply.github.com>
2023-04-19 16:44:17 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,30 +1,27 @@
 function solution(keyinput, board) {
-  	// 1. x,y 좌표를 선언한다.
-    let x = 0;
-    let y = 0;
+   //1.x와 y의 변수를 정해준다.
+    let x=0;
+    let y=0;
     
-  	// 2. board의 범위도 설정한다. 
-    const range = 
-          [ Math.abs( Math.floor(board[0] / 2) ), 
-           Math.abs( Math.floor(board[1] / 2) ) ];
-    
-  	// 3. keyinput에 방향이 입력된다면 그에 맞게 x 또는 y 좌표를 변경한다.
+    //2.범위를 설정해준다.
+    const range = [Math.abs(Math.floor(board[0] / 2 )),
+                  Math.abs(Math.floor(board[1] / 2))]
+    //3.key input을 넣을때의 경우의 수를 넣어준다
     for(let k of keyinput){
         switch(k){
-            case "left": x--; break;
-            case "right": x++; break;
-            case "down": y--; break;
-            case "up": y++; break;
+            case "left" : x--;break;
+            case "right" : x++;break;
+            case "up" : y++;break;
+            case "down" : y--;break;
         }
-        
-      	// 4. x와 y의 값이 설정 범위를 넘어간다면 설정된 범위의 최댓값을 대입한다.
+        //4.최대 값의 범위를 설정해준다.
         if(Math.abs(x)>range[0]){
-            x = x > 0 ? range[0] : range[0] * -1;
-         }
-        if(Math.abs(y)>range[1]){
-            y = y > 0 ? range[1] : range[1] * -1;
+            x=x>0 ? range[0]:range[0]*-1;
         }
-        
+        if(Math.abs(y)>range[1]){
+            y=y>0 ? range[1]: range[1]*-1;
+            
+        }
     }
-    return [x,y];
+    return [x,y]
 }
