f44f778bde609fa1e6ed2bcf1ceee4d1fd3e28b5
정채림 <119480528+ellin45@users.noreply.github.com>
2023-04-25 14:40:54 +0900

    [unrated] Title: 배열 만들기 5, Time: 1.71 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -0,0 +1,11 @@
+function solution(intStrs, k, s, l) {
+    const result = [];
+    for (let i = 0; i < intStrs.length; i++) {
+        const substr = intStrs[i].substr(s, l);  // s번 인덱스에서 시작하는 길이 l짜리 부분 문자열 추출
+        const num = parseInt(substr, 10);  // 추출한 부분 문자열을 정수로 변환
+        if (num > k) {  // 변환한 정수가 k보다 큰 경우
+            result.push(num);  // 결과 배열에 추가
+        }
+    }
+    return result;
+}
