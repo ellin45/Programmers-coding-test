3a6ee4d5a4602a23fe3d334cb4124ec3bcd5adf4
정채림 <119480528+ellin45@users.noreply.github.com>
2023-05-01 15:29:50 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,18 +1,20 @@
 function solution(n) {
     let answer = 0;
-
-    const checkNumber = (k)=>{
-        const numbers = String(k).split("")
-        if(k%3===0|| numbers.includes("3")){
-            answer++
-            checkNumber(answer)
-    	} else {
-        return
-    	}
+    
+    const checkNumber = (k) =>{
+       const numbers = String(k).split("")
+       //k가 3의 배수이거나 3을 포함하고 있다면 1을 더해준다
+       if(k % 3 === 0 || numbers.includes("3")){
+           answer++;
+           checkNumber(answer)
+       }else{
+          return
+       }
     }
+    //반복문을 돌때마다 무조건 1씩 추가한다.
     for(let i = 0; i<n; i++){
         answer++
         checkNumber(answer)
     }
     return answer
-}
+    }
