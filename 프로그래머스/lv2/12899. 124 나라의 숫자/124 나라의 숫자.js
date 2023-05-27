1446f204bb12d9744a2968c3c1867aebf051829a
정채림 <119480528+ellin45@users.noreply.github.com>
2023-05-27 21:32:16 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.js
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -8,9 +8,11 @@ function solution(n) {
     
     while(true) {
         if(n === 0) break
-        
-        result = oneTwoThreeMap[n % 3] + result;
-        n = (n % 3 === 0) ? Math.floor(n / 3-1) : Math.floor(n /3);
-    }
+    
+    
+    result = oneTwoThreeMap[n%3] + result;
+    
+    n = (n % 3 === 0 ) ? Math.floor(n / 3 -1) : Math.floor(n / 3);
+}
     return result;
 }
