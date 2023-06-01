5661b84b3fe05c989b4c098475829d87396ac2f4
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-01 11:39:21 +0900

    [level 2] Title: 괄호 변환, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
@@ -0,0 +1,48 @@
+function solution(p) {
+    let answer = '';
+    let open = 0;
+    let close = 0;
+    
+    //1. 재귀 탈출 조건
+    if(!p) return '';
+    
+    for(let i=0; i<p.length; i++){
+        if(p[i] === '(') open++
+        else close++
+    
+    //균형 일때 "올바른 괄호 문자열"인지 아닌지 판단
+    if(open === close){
+        //2,3,3-1
+        if(check(p.slice(0, i+1))){
+            answer = p.slice(0, i+1) + solution(p.slice(i+1));
+            return answer;
+        } else {
+            //4-1, 4-2, 4-3
+            answer = '(' + solution(p.slice(i+1)) + ')';
+            
+            //4-4
+            for(let j=1; j < i; j++){
+                if(p[j] === '('){
+                    answer = answer + ')'
+                }else {
+                    answer = answer + '('
+                }
+            }
+            return answer;
+            }
+        }
+    }
+}
+
+// 올바른 괄호 문자열인지 판단하는 함수
+function check(p) {
+    let stack = [];
+    for(let i=0; i<p.length; i++){
+        if(p[i] === '(') stack.push('(')  
+        else {
+            if(stack.length === 0) return false;
+            stack.pop();
+        }
+    }
+    return true;
+}
