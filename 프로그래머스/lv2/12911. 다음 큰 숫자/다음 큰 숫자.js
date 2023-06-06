ef33a5c17ae33304265f610ba9e1582997407f11
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-06 16:24:46 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,23 +1,24 @@
 function solution(n) {
-    let numbers = '';
-    let nextNum = 0;
+    let numbers = n.toString(2);
+    let countOfOneInN = [...numbers].filter((el) => '1' === el).length;
+
     
-    numbers = n.toString(2);
-    
-    while(true){
-    numbers = [...numbers].filter((el) => '1' === el).length
-  
-        if(numbers.length === nextNum.length){
-            numbers++
-            console.log(nextNum)
+    while (true) {
+        n++;
+        let binaryOfNextNum = n.toString(2);
+        let countOfOneInNextNum = [...binaryOfNextNum].filter((el) => '1' === el).length;
+        
+        if (countOfOneInN === countOfOneInNextNum) {
+            return n;
         }
-        break;
     }
-    return numbers;
 }
+
+
+
 //n을 toString으로 2진법으로 바꾼다.
      //문자열로 변환한다
     //문자열에 0이있다면 제거하고
-//n과 다음 문자열의 길이가 같다면 같은 숫자이다.
+//n과 다음 문자의 문자열의 길이가 같다면 같은 숫자이다.
 
 
