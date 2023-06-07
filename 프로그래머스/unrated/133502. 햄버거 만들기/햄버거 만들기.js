bb8175baedc52b1f0da8f1e96f89a77868f312f5
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-07 11:03:51 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,26 +1,21 @@
 function solution(ingredient) {
-    const burger = [1, 2, 3, 1];
-    let stack = [];
-    let count = 0;
+  const burger = [1, 2, 3, 1];
+  let stack = [];
+  let count = 0;
 
-    ingredient.forEach(item => {
-        stack.push(item);
-        while (stack.length >= 4) {
-            // 스택의 상단 4개 요소를 가져와서 burger와 일치하는지 확인합니다.
-            const top4 = [stack[stack.length - 4], stack[stack.length - 3], stack[stack.length - 2], stack[stack.length - 1]];
-            if (burger.every((value, index) => value === top4[index])) {
-                // 햄버거 패턴이 일치하면, 상단 4개 요소를 제거하고 카운트를 증가시킵니다.
-                stack.pop();
-                stack.pop();
-                stack.pop();
-                stack.pop();
-                count++;
-            } else {
-                // 햄버거 패턴이 일치하지 않으면, 루프를 중지합니다.
-                break;
-            }
-        }
-    });
+  ingredient.forEach(item => {
+    stack.push(item);
+    while (stack.length >= 4) {
+      const top4 = stack.slice(-4).join('');
+      const burgerPattern = burger.join('');
+      if (top4 === burgerPattern) {
+        stack.splice(-4);
+        count++;
+      } else {
+        break;
+      }
+    }
+  });
 
-    return count;
+  return count;
 }
