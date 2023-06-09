4d07f886e05cdac18cdb356bfc4bd3c429ccce2f
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-09 21:34:51 +0900

    [level 0] Title: 옹알이 (1), Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,11 +1,21 @@
-function solution(babbling){
-    let answer = 0;
-    
-    for(let i=0; i<babbling.length;i++){
-        if(babbling[i].replace("ye"," ").replace("aya"," ").replace("woo"," ").replace("ma"," ").trim() ===""){
-            answer = answer + 1;
+function solution(babbling) {
+    // const babyLanguage = ["aya","ye","woo","ma"]
+    //매치를 써서 되는지 확인을 해보쟈
+   const regex = /^(aya|ye|woo|ma)+$/;
+     let cnt = 0;
+    //이렇게 확인하고
+    //맞는지 순회를돌면서 확인하자.
+    //먼저 babbling안에 있는 regex조건을 만족하는 애들을 하나씩 맞다면 cnt를 올려주면 되지않을까
+    babbling.forEach((value)=>{
+        if(regex.test(value)){
+            cnt++;
         }
-    }
-    return answer;
-    }
+    })
+   
+    //하나씩 비교해주는거 아닌가
+    //그냥 replaceAll 해서 풀어야되나욤
     
+    return cnt; 
+}
+//babbling은 최대 한번씩만 등장합니다.
+
