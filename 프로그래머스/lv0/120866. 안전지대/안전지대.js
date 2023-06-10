f151938e1e7e7e0cb25bdfef56e53410d5814f16
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-10 18:15:18 +0900

    [level 0] Title: 안전지대, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,46 +1,38 @@
 function solution(board) {
-    let result = 0
-    // 전체 순회
-    for(let i = 0 ; i < board.length ; i ++) {
-        for(let j = 0 ; j < board[i].length ; j ++) {
-            // board[i][j]의 값이 1인경우 상하좌우 및 대각선 검사를 실행
-            if(board[i][j] === 1) {
-                // 상하 좌우를 2로 변경하되 이는 해당 칸이 0인 경우에만 해당 즉 폭탄은 건들지 않는다.
-                // 맨 윗줄이 아닌 경우
-                if(i !== 0 && board[i-1][j] !== 1) {
-                    board[i-1][j] = 2    
+    const n = board.length;
+    const dangerZone = Array.from(Array(n), () => new Array(n).fill(0));
+
+    // 상, 하, 좌, 우, 좌상, 우상, 좌하, 우하 방향을 나타내는 리스트
+    const dx = [-1, 1, 0, 0, -1, -1, 1, 1];
+    const dy = [0, 0, -1, 1, -1, 1, -1, 1];
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            // 지뢰가 있으면
+            if (board[i][j] === 1) {
+                for (let k = 0; k < 8; k++) {
+                    let nx = i + dx[k];
+                    let ny = j + dy[k];
+
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                        // 지뢰 주변 영역을 위험지역으로 표시
+                        dangerZone[nx][ny] = 1;
+                    }
                 }
-                // 맨 아랫줄이 아닌 경우
-                if(i !== board.length-1 && board[i+1][j] !== 1) {
-                    board[i+1][j] = 2
-                }
-                // 맨 왼쪽이 아닌 경우
-                if(j !== 0 && board[i][j-1] !== 1) {
-                    board[i][j-1] = 2
-                }
-                // 맨 오른쪽이 아닌 경우
-                if(j !== board[i].length-1 && board[i][j+1] !== 1) {
-                    board[i][j+1] = 2
-                }
-                // 맨 대각선 윗 왼쪽이 아닌 경우
-                if(i !== 0 && j !== 0 && board[i-1][j-1] !== 1) {
-                    board[i-1][j-1] = 2
-                }
-                // 맨 대각선 윗 오른쪽이 아닌 경우
-                if(i !== 0 && j !== board[i].length-1 && board[i-1][j+1] !== 1) {
-                    board[i-1][j+1] = 2
-                }
-                // 맨 대각선 아랫 왼쪽이 아닌 경우
-                if(i !== board.length-1 && j !== 0 && board[i+1][j-1] !== 1) {
-                    board[i+1][j-1] = 2
-                }
-                // 맨 대각선 아랫 오른쪽이 아닌 경우
-                if(i !== board.length-1 && j !== board[i].length-1 && board[i+1][j+1] !== 1) {
-                    board[i+1][j+1] = 2
-                }
+                // 지뢰 위치 자체도 위험지역으로 표시
+                dangerZone[i][j] = 1;
             }
         }
     }
-    board.forEach(a => a.forEach(b => b === 0 ? result++ : null))
-    return result
+
+    let safeZone = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (dangerZone[i][j] === 0) {
+                safeZone += 1;
+            }
+        }
+    }
+
+    return safeZone;
 }
