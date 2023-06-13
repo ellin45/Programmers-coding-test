f0fdc1acf01d628a2b21557cec9082bb12030e43
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-13 11:41:58 +0900

    [level 0] Title: 저주의 숫자 3, Time: 6.01 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,20 +1,34 @@
 function solution(n) {
-    let answer = 0;
-    
-    const checkNumber = (k) =>{
-       const numbers = String(k).split("")
-       //k가 3의 배수이거나 3을 포함하고 있다면 1을 더해준다
-       if(k % 3 === 0 || numbers.includes("3")){
-           answer++;
-           checkNumber(answer)
-       }else{
-          return
-       }
+  // list를 만들고 숫자를 넣자
+  // 1부터 ++ 되는 increaseNum 을 선언하고 3마을에서 쓰일수 있을때 업데이트
+  // 1부터 25까지 돌아야하니까 count라는 변수를 1로 선언해서 증가시켜주자
+  // 반복문
+  // 종료조건 -> if : n === count -> 왜? 1부터 25까지 돌릴건데 25가 되면 더이상 돌릴필요 x
+  //3의 배수와 숫자 3을 사용하지 않습니
+  // if : String(n).includes(3) || n % 3 === 0 -> continue 넘어가야해
+  // 추가 가능할때
+  // else : list.push(넣을 수 있는 숫자)
+  // 인덱스 자체가 10진법 숫자입니다.
+  let villageNums = [];
+
+  let count = 0;
+  let increaseNum = 1;
+  while (true) {
+    // 종료조건
+    if (n === count) break;
+
+    // 추가못할때 if : String(n).includes(3) || n % 3 === 0 -> continue 넘어가야해
+    if (String(increaseNum).includes(3) || increaseNum % 3 === 0) {
+      increaseNum++;
+      continue;
+    } else {
+      // 추가 가능할때 1, 2, (3은 안됨), 4, 5, (6은안됨), 7, 8, (9는 안됨), ...
+      villageNums.push(increaseNum);
+      increaseNum++;
+      console.log(villageNums);
     }
-    //반복문을 돌때마다 무조건 1씩 추가한다.
-    for(let i = 0; i<n; i++){
-        answer++
-        checkNumber(answer)
-    }
-    return answer
-    }
+    count++;
+  }
+
+  return villageNums.pop();
+}
