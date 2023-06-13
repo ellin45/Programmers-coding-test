7eb7bc7bbbb01be9b4ac513ddc63a91a6a9600f0
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-14 01:55:54 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 65.21 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,17 +1,20 @@
 function solution(s) {
     let result = [];
-    let tmp = [];
+    let lastPos = {};
     
-    for(let i = 0; i < s.length; i++){
-        if(!tmp.includes(s[i])){
+    s.split("").forEach((char, i) => {
+        if (char in lastPos) {
+            result.push(i - lastPos[char]);
+        } else {
             result.push(-1);
-            tmp.push(s[i]);
-        } else if(tmp.includes(s[i])){
-            result.push(i - tmp.lastIndexOf(s[i]));
-            tmp.push(s[i]);
         }
-        
-    }
+        lastPos[char] = i;
+        console.log(lastPos[char],i)
+    });
     return result;
 }
 
+//스택을 쌓으면서 앞 요소에 나와 같은 원소가 있는지 확인한다.
+//s의 요소들을 하나씩 배열에 넣어줍니다.
+//앞의 요소들을 확인했을떄 중복 값이 있다면 2
+    //없다면 -1
