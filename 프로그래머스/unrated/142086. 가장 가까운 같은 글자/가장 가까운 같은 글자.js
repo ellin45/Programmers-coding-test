085571f24b1351d621025b113e57721af9a50582
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-14 11:45:59 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 59.66 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,20 +1,10 @@
-function solution(s) {
-    let result = [];
-    let lastPos = {};
-    
-    s.split("").forEach((char, i) => {
-        if (char in lastPos) {
-            result.push(i - lastPos[char]);
-        } else {
-            result.push(-1);
-        }
-        lastPos[char] = i;
-        console.log(lastPos[char],i)
-    });
-    return result;
-}
+const solution = (s) =>
+  [...s].map((char, i) => {
+    const count = s.slice(0, i).lastIndexOf(char);
+      console.log(count,i, char)
+    return count < 0 ? count : i - count;
+      
+  });
 
-//스택을 쌓으면서 앞 요소에 나와 같은 원소가 있는지 확인한다.
-//s의 요소들을 하나씩 배열에 넣어줍니다.
-//앞의 요소들을 확인했을떄 중복 값이 있다면 2
-    //없다면 -1
+//lastIndexOf() === 일치하는 인덱스가 없을 경우 -1을반환
+//그 문자의 인덱스를 반환하고, 이 때 count 값은 그 인덱스가 됩니다
