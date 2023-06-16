16d1c6cf6f031c3c85d229ca08092e806114dd1e
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-16 19:45:44 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.11 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,20 +1,32 @@
-function solution(number, limit, power) {
-  let answer = 0;
+function divisor(n) {
+  let cnt = 0;
 
-  for (let i = 1; i <= number; i++) {
-    let divisor = 0;
-    for (let j = 1; j <= Math.sqrt(i); j++) {
-      if (i % j === 0) {
-        if (i / j === j) divisor += 1;
-        else divisor += 2;
-      }
-      if (divisor > limit) {
-        divisor = power;
-        break;
-      }
-    }
-    answer += divisor;
+  for (let i = 1; i * i <= n; i++) {
+    if (i * i === n) cnt++;
+    else if (n % i === 0) cnt += 2;
   }
-  
-  return answer;
+  return cnt;
 }
+
+function solution(number, limit, power) {
+    let temp = [];
+    
+    for(let i=1; i<=number; i++) temp.push(divisor(i));
+    
+    return temp.reduce((acc,cur)=>{
+        if(cur>limit) acc+=power;
+        else acc+=cur;
+        return acc;
+    },0);
+}
+
+
+//공격력의 한계는 3
+//약수의 개수는 3개까지 가능
+//공격력에 따라 필요한 무게가 달라짐
+//무게 = power * divisor.slice(0,limit)
+
+
+
+
+
