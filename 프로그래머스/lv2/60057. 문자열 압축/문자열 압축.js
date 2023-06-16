5b27ea33618a615a29cff1ab673d37d1a1822870
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-16 11:07:35 +0900

    [level 2] Title: 문자열 압축, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -3,11 +3,11 @@ function solution(s) {
 
     for (let i = 1; i <= Math.floor(s.length / 2); i++) {
         let compressed = '';
-        let prev = s.substring(0, i);
+        let prev = s.slice(0, i);
         let count = 1;
 
         for (let j = i; j < s.length; j += i) {
-            let substr = s.substring(j, j + i);
+            let substr = s.slice(j, j + i);
             if (prev === substr) {
                 count++;
             } else {
@@ -23,14 +23,3 @@ function solution(s) {
 
     return min_length;
 }
-
-
-
-    //스택을 하나씩 쌓는다 하나씩 자른 문자열을~ stack에 아무것도 없다면?
-    //문자열을 push한다.
-    //하나씩 쌓을때마다 가장 뒤에 있는 문자랑 같으면 하나를 없애주고(pop) cnt를 증가한다.
-    //가장 뒤에 있는 문자랑 다르면 하나를 push해준다.
-    //스택에 쌓인 문자열의 개수를 반환하고
-    //나머지 문자열을 붙여준다.
-    //모두 붙힌뒤 그 문자열의 길이를 반환한다.
-    // return answer;
