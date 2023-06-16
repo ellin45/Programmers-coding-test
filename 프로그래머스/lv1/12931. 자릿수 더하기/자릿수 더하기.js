a0d33f2c20fc11060b094eba557c4b0542777f92
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-16 11:45:50 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,11 @@
 function solution(n)
 {
-    var answer = 0;
-    var a =String(n);
-    for(var i=0; i<a.length; i++){
-        answer += parseInt(a[i]);
-        console.log(answer);
-    };
-    return answer;
+   return(n+"").split("").reduce((acc,curr) => acc + parseInt(curr),0)
+     
 }
+
+// var answer = 0;
+//     var a =String(n);
+//     for(var i=0; i<a.length; i++){
+//         answer += parseInt(a[i]);
+//         console.log(answer);
