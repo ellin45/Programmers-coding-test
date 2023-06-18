fa7af0f0a2e456ac36a6c4be5aa9a53acff58364
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-18 21:02:04 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,22 +1,14 @@
-function solution(brown, yellow) {
-// arr.map(callback(currentValue[, index[, array]])[, thisArg])
-// 이 조합이 갈색 타일과 일치하는지 검사하는 함수
-const isMatch = (width, height) => {
-// 규칙 : 노란색 타일의(가로 길이 + 세로 길이) * 2 + 4 = 갈색 타일 개수
-const x = (width + height) * 2 + 4;
+// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
+// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
+function solution(brown, red) {
+    var answer = [];
+    for (var i = 3; i <= (brown+red)/i; i++) {
+        var x = Math.floor((brown+red)/i);
+        if( (x-2)*(i-2)=== red) {
+            break;
+        }
+    }
 
-if(x === brown) return true;
+    return [x,i];
 }
 
-// yellow가 24인 경우 나올 수 있는 타일 조합
-// => (4x6) = 24 / (3x8) = 26 / (2x12) = 32 / (1x24) = 54
-for(let i = yellow; i >= 0; i--){
-
-if((yellow % i) === 0) {
-
-if(isMatch(i, yellow / i)) {
-return [i+2, (yellow/i) + 2]
-};
-}
-}
-}
