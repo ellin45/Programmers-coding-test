752fa0bbcc33d8782d85cb779d790fc80664d50f
정채림 <119480528+ellin45@users.noreply.github.com>
2023-06-28 23:42:44 +0900

    [level 1] Title: 평균 구하기, Time: 4.16 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -2,6 +2,8 @@ function solution(arr) {
     var answer = 0;
     for(let i=0; i <arr.length; i++){
         answer += arr[i]; 
+            console.log(arr[i])
     }
     return answer/arr.length;
+
 }
