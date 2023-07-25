185264611471f8e503bac2f6ceff8df062ed2430
정채림 <119480528+ellin45@users.noreply.github.com>
2023-07-25 23:57:44 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,5 +1,3 @@
-// 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
-// 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
 function solution(brown, red) {
     var answer = [];
     for (var i = 3; i <= (brown+red)/i; i++) {
@@ -12,3 +10,13 @@ function solution(brown, red) {
     return [x,i];
 }
 
+function solution(brown, red){
+    var answer = [];
+    for(var i=0; i<=(brown + red)/i; i++){
+        var x= Math.floor((brown + red)/i);
+        if((x-2)*(i-2)===red){
+            break;
+        }
+    }
+    return[x,i]
+}
