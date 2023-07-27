fc5b658a8e14a44b6e518c8dbf789a0fa64595dd
정채림 <119480528+ellin45@users.noreply.github.com>
2023-07-27 20:53:42 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,4 +1,5 @@
 function solution(nums) {
+   
     let answer = 0;
     const len = nums.length;
 
@@ -13,9 +14,9 @@ function solution(nums) {
     return answer;
 }
 
-function isPrime(sum) {
-    for(let i = 2; i < sum; i++)
-    if(sum % i === 0) return false;
-    return sum > 1;
+function isPrime(sum) { // 소수인지 판별하는 함수
+    for(let i = 2; i < sum; i++)  // { 2부터 합계까지 더해 하나씩 }
+    if(sum % i === 0) return false; //합계값이 인덱스로 나누었을때 0이면 소수야
+    return sum > 1; // 합계값이 1보다 크면 return 해
 }
-        
+
