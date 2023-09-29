c31f81830c4fafe2c87de9d7593048cbb6aa3a76
정채림 <119480528+ellin45@users.noreply.github.com>
2023-09-29 13:03:15 +0900

    [unrated] Title: 배열 조각하기, Time: 0.13 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -1,19 +1,24 @@
 function solution(arr, query) {
-query.forEach((value, i) => {
-// current idx : 0 -> query[i] === value === 4
-// query를 순회하면서 다음 작업을 반복합니다.
-// 짝수 인덱스에서는 {arr에서} (query[i] === value)4번 인덱스를 제외하고 배열의 4번 인덱스 뒷부분을 잘라서 버립니다.
-// 홀수 인덱스에서는 arr에서 1번 인덱스는 제외하고 배열의 1번 인덱스 앞부분을 잘라서 버립니다.
-// [0, 1, 2, 3, 4]
-if (i % 2 === 0) {
-// 4 우리는 3개를 짤라야해
-arr.splice(value + 1) // 0번째부터 value + 1개까지 짤라줘
-// arr.slice(2, 2) -> 두번째부터 두번째까지 -> 1개
-// arr.splice(2, 2) -> 2번째부터 2개 ->
-} else {
-arr = arr.slice(value)
+    //arr가 query를 순회하면서 작업을 반복한다.
+       query.forEach((el,i)=> {
+    //arr가 0부터 하나씩 증가하면서 짝수인지 홀수인지에 따라 
+        //짝수라면 arr의 arr의 뒷부분을 자른다.
+           if(i% 2 === 0){
+               arr.splice(el+1)
+           }else{
+            arr = arr.slice(el);
+        } 
+       })
+    return arr;
 }
-});
-return arr;
-}
-    
+            // 만약 짝수인 0번의 인덱스의 쿼리라면 제일 마지막인 -1의 인덱스를 자른다
+                //대신 query의 [0]번째 인덱스는 제외하고 자른다.
+                // 즉4번째 인덱스는 제외하고 자른다.
+                // 그러나 4번째 인덱스가 없으니 제외하고 넘어간다.
+        //홀수라면 arr의 query[i]앞부분을 자른다.
+            //만약 홀수인 1번의 인덱스의 쿼리라면 제일 첫번째인 arr의 1번째 인덱스를 자른다.
+  
+    //이 과정을 arr의 길이만큼 반복한다.
+    //작업이 끝나면 남은 arr의 부분배열을 return한다.
+        //남은 arr의 부분배열 === 남은 arr
+
