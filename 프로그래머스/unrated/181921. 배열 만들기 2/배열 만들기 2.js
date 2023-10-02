27c05a3c1aa803e80139d5ab7dd67312b16fddf2
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-02 22:22:48 +0900

    [unrated] Title: 배열 만들기 2, Time: 7.56 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,28 +1,11 @@
 function solution(l, r) {
-    let result = [];
-
-    // Start from "5", generate numbers by appending "0" and "5"
-    let str = ["5"];
-    while (str.length > 0) {
-        let numStr = str.shift();
-        let num = Number(numStr);
-
-        if (num >= l && num <= r) {
-            result.push(num);
+    let answer = [];
+    for(let i=l; i<=r; i++){
+        let str = i.toString();
+        if(/^[05]+$/.test(str)){ 
+            answer.push(i);
         }
-
-        // Do not generate numbers beyond the upper limit
-        if (num > r) continue;
-
-        // Append "0" and "5" to generate more numbers
-        str.push(numStr + "0");
-        str.push(numStr + "5");
     }
-
-    // Return [-1] if no number is found
-    if (result.length === 0) {
-        result.push(-1);
-    }
-
-    return result;
+    if(answer.length === 0) answer.push(-1); 
+    return answer;
 }
