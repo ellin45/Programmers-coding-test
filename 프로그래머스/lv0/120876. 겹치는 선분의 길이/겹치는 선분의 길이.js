402e8c8580b909a26838aa010b6d8bdfc71f3456
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-06 01:56:31 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,52 +1,35 @@
 function solution(lines) {
-//겹치는 선분의 길이를 알려면 [start,end]라고 했을때
-//x부터 y까지로 늘어뜨린 배열 하나씩이랑 서로 겹치는 숫자가 몇개 있는지가 중요한거같다.
-//그럼 x부터 y까지 나올수 있는 숫자를 뽑아서 그 숫자끼리 중복값이 몇개인지를 알면 되지 않나욤
-//구조분해 할당을 lines를 [start,end]를 해보자
-//map을 이용해 보래여어
-//map을 어떻게 사용하냐면 서로 선분끼리 겹치는걸 만들어야하거든여 점은 신경쓰지말고 그러니까 시작점은 포함하지말고 끝점만 +1을 해주는거야
-let map = new Map();
+    // 시작점과 끝점을 이벤트로 변환
+    let events = [];
+    for (let line of lines) {
+        events.push({point: line[0], type: "start"});
+        events.push({point: line[1], type: "end"});
+    }
 
-// {
-// -3 : 1,
-// -2 : 1,
-// -1 : 1,
-// }
-// map.set('키', '내가 할당하고 싶은 값 == value')
-// map.set(-3, 1)
-// map.get('키') -> '키'에 해당하는 값(result)를 가져올거야
+    // 이벤트를 정렬
+    events.sort((a, b) => {
+        if (a.point === b.point) {
+            return a.type === "start" ? -1 : 1;  // 시작점을 끝점보다 앞에 둔다.
+        }
+        return a.point - b.point;
+    });
 
-lines.forEach((value, idx) => {
-// 1번쨰, 2번째, 3번째
-// [ [0, 2], [-3, -1], [-2, 1] ]
-// current Value : [ 0, 2 ]
-let [start, end] = value;
-// i : -2 ~ 0
-for (let i = start; i < end; i++) {
-// i = -2
-// map에 넣고싶은데 이미 i 라는 키를 가진 value가 있어
-// 그럼 그 값에 + 1 해주자
-// 없어 ! -> map에서 get으로 가져올때 아무것도없으면 undefined -> 부정연산자 !undefined => true
-// 경우의 수가 2개야 있고 없고.
-if (!map.get(i)) {
-map.set(i, 1);
-} else if (map.get(i)) {
-// i라는 키에 할당한 값이 있을때
-map.set(i, map.get(i) + 1);
-}
-}
-});
+    let overlapLength = 0;
+    let active = 0; // 현재 활성화된 선분의 수
+    let prevPoint = null;
 
-// key : 좌표 (x축)
-// value : 각 x 가 몇번 등장했냐 -> 겹친곳이다. -> 등장빈도
-// 두 개 이상의 선분이 겹치는 부분의 길이를 return 하도록 solution 함수를 완성해보세요.
-// 두개 이상이 몇번 등장했냐? 길이를 return 하도록 solution 함수를 완성해보세요.
-// arr.forEach((value, idx, selfArr) => {})
-const countArr = [];
-map.forEach((value, key, idx, selfArr) => {
-if (value >= 2) {
-countArr.push(key);
+    for (let event of events) {
+        if (active >= 2 && prevPoint !== null) { // 2개 이상의 선분이 겹치는 경우만 고려
+            overlapLength += event.point - prevPoint;
+        }
+        if (event.type === "start") {
+            active++;
+        } else {
+            active--;
+        }
+        prevPoint = event.point;
+    }
+
+    return overlapLength;
 }
-});
-return countArr.length;
-}
+
