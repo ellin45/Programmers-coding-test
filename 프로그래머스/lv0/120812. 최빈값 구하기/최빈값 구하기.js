af53d73f14575fe9e3781e8902779d0e4a80b684
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-05 21:55:49 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,23 +1,18 @@
 function solution(array) {
-    let map = new Map();
-    let max = 0;
-    //map을 만드는 이유
-    //키와 값을 비교해서 키에 해당하는 값 : count를 세어줄거야
-    array.forEach((el)=> map.set(el ,(map.get(el) || 0) +1))
-  
-    for(let times of map.values()){
-        max = Math.max(max, times)
+    let mode = new Map();
+
+    array.forEach((el) => {
+        mode.set(el, (mode.get(el) || 0) + 1);
+    });
+
+   
+    let sortedPairs = [...mode.entries()].sort((a, b) => b[1] - a[1]);
+
+
+    if (sortedPairs.length === 1 || sortedPairs[0][1] > sortedPairs[1][1]) {
+        return sortedPairs[0][0];
+    } else {
+        return -1;
     }
-    let ans = [];
-    
-    //key값을 돌면서 최빈값과 같은 값을 찾아줍니다.
-    [...map].forEach((el)=>{
-        let [num, times] = el
-        if(times === max){
-            ans.push(num);
-        }
-    })
-    //value 길이가 1인 것은 1을 반환한다. 
-    if(ans.length === 1) return ans[0];
-    return -1;
 }
+
