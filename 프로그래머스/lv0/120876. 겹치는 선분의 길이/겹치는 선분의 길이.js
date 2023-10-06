96720f77682eef4cc5219939479c3b3c5ab9da0c
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-06 11:30:12 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,35 +1,25 @@
 function solution(lines) {
-    // 시작점과 끝점을 이벤트로 변환
-    let events = [];
-    for (let line of lines) {
-        events.push({point: line[0], type: "start"});
-        events.push({point: line[1], type: "end"});
-    }
-
-    // 이벤트를 정렬
-    events.sort((a, b) => {
-        if (a.point === b.point) {
-            return a.type === "start" ? -1 : 1;  // 시작점을 끝점보다 앞에 둔다.
+    
+    //배열로 만들어주자 뭐를? 세 선분을 start와 end로 구분된..
+    let map = new Map();
+    lines.forEach((item) =>{
+        const [start, end] = item;
+        
+        for(let i=start; i<end; i++){
+            //map에 넣고 싶은데 이미 i라는 키를 가진 value가 있어 그럼 그 값에 +1를 해주자
+            if(!map.get(i)){
+                map.set(i,1)
+            }else if(map.get(i)){
+                map.set(i,map.get(i)+1);
+            }
         }
-        return a.point - b.point;
     });
-
-    let overlapLength = 0;
-    let active = 0; // 현재 활성화된 선분의 수
-    let prevPoint = null;
-
-    for (let event of events) {
-        if (active >= 2 && prevPoint !== null) { // 2개 이상의 선분이 겹치는 경우만 고려
-            overlapLength += event.point - prevPoint;
+    const countArr = [];
+    map.forEach((value,key,idx,selfArr) =>{
+        if(value >=2){
+            countArr.push(key);
         }
-        if (event.type === "start") {
-            active++;
-        } else {
-            active--;
-        }
-        prevPoint = event.point;
-    }
-
-    return overlapLength;
+    })
+    
+    return countArr.length;
 }
-
