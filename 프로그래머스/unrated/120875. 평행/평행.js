a70147f5a6f508fe0e9faad2330d2e20bc61c837
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-15 23:48:25 +0900

    [level unrated] Title: 평행, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120875. 평행/평행.js
+++ 프로그래머스/unrated/120875. 평행/평행.js
@@ -1,20 +1,4 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
 function solution(dots) {
-    dots.length = 4;
     let [[a,b],[c,d],[e,f],[g,h]] = dots
 
     let slope = parseFloat((f-b))/parseFloat((e-a));
