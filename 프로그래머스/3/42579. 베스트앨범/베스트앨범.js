8b0e5f0c7c9536a31948b480c2a34802835e8daa
정채림 <119480528+ellin45@users.noreply.github.com>
2023-12-01 21:58:56 +0900

    [level 3] Title: 베스트앨범, Time: 4.37 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -3,30 +3,42 @@ function solution(genres, plays) {
   let ranking = {};
 
   // 2. 각 장르의 총 재생 횟수를 계산하여 ranking 객체에 저장
-  genres.forEach((genre, idx) => {
-    if (!ranking[genre]) {
-      ranking[genre] = 0;
+genres.forEach((genre,idx)=>{
+    if(!ranking[genre]){
+        ranking[genre] = 0;
     }
+    // console.log("ranking더하기전",!ranking[genre])
     ranking[genre] += plays[idx];
-  });
+    // console.log(ranking[genre]);
+})
+ 
 
   // 3. ranking 객체를 재생 횟수에 따라 내림차순으로 정렬
-  const sortedGenres = Object.keys(ranking).sort((a, b) => ranking[b] - ranking[a]);
+ let sortedgenre = Object.keys(ranking).sort((a,b)=>
+     ranking[b]-ranking[a])
 
   // 4. 각 장르별로 해당하는 노래들을 재생 횟수에 따라 내림차순으로 정렬하고,
   //    고유 번호를 배열에 담아 반환
-  const result = [];
-  sortedGenres.forEach((genre) => {
-    const genreIndexes = [];
-    for (let i = 0; i < genres.length; i++) {
-      if (genres[i] === genre) {
-        genreIndexes.push(i);
-      }
-    }
-
-    genreIndexes.sort((a, b) => plays[b] - plays[a]);
-    result.push(...genreIndexes.slice(0, 2)); // Adjust the slice count based on your requirements
-  });
-
-  return result;
+ 
+const result = [];
+    sortedgenre.forEach((genre)=>{
+        const genreIndexes = [];
+        for(let i = 0; i < genres.length; i++){
+            if(genres[i] === genre){
+                genreIndexes.push(i);
+                console.log("genres[i]",genres[i]);
+                console.log("genre",genre);
+                console.log("genreIndexes",genreIndexes);
+                
+            }
+        }
+        
+        genreIndexes.sort((a,b)=> plays[b]-plays[a]);
+        console.log("genreIndexes2",genreIndexes);
+        
+        result.push(...genreIndexes.slice(0,2));
+        console.log("genreIndexes3",genreIndexes);
+        
+    })
+ return result
 }
