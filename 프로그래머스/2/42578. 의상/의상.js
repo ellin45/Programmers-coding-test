9a76497dbf3967b899ba67a48ef374edbd2bdc8e
정채림 <119480528+ellin45@users.noreply.github.com>
2023-12-01 17:20:37 +0900

    [level 2] Title: 의상, Time: 7.97 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -3,18 +3,18 @@ function solution(clothes) {
   let result = 1;
 
   // 의상 종류별 개수 count
-  clothes.forEach((v) => {(types[v[1]] = (types[v[1]] || 0) + 1) });
+  clothes.forEach((v) => {
+      (types[v[1]] = (types[v[1]] || 0) + 1) 
+      console.log(types);
+  });
 
   // (종류A + 입지 않는 경우) * (종류B + 입지 않는 경우) * ...
   // (A+1) * (B+1) * ...
-  for (const key in types) {
-    result *= types[key] + 1;
-  }
+    for(const key in types){
+        result *= types[key]+1;
+        console.log(result);
+    }
 
   // 아무 것도 입지 않는 경우 제외 후 return
   return result - 1;
 }
-// 같은 의상종류는 겹쳐서 입을수 없음
-// 하나의 의상종류는 하나만 입을수 있음
-// 해시에서 키값은 의상종류 값은 의상이름
-//
